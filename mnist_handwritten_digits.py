20e37c4c2eca29cc994a0574ebd71649357e1cc0
piechocki <34336439+piechocki@users.noreply.github.com>
2019-06-14 15:48:14 +0200

    Add own implementation of part 2 of the book

---
+++ mnist_handwritten_digits.py
@@ -0,0 +1,60 @@
+import numpy as np
+import scipy.special
+
+class neuralNet:
+
+    def __init__(self, inputnodes, hiddennodes, outputnodes,
+    learningrate = 0.1):
+        # save number of nodes and the learning rate in the class
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.lr = learningrate # aka alpha
+
+        # define a class wide sigmoid function, here a logistical fuction
+        # with y=1/(1+e^(-x))
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+        # get the weight matrices with initial values
+        # theory says:
+        # choose a random number within +-1/sq.root(number of outgoing nodes)
+        self.w_ih = np.random.normal(0.0, pow(self.hnodes, -0.5),
+                                     (self.hnodes, self.inodes))
+        self.w_ho = np.random.normal(0.0, pow(self.onodes, -0.5),
+                                     (self.onodes, self.hnodes))
+        # alternatively you can choose a random number within +-0.5
+        # self.w_ih = np.random.rand(self.hnodes, self.inodes) - 0.5
+        # self.w_ho = np.random.rand(self.onodes, self.hnodes) - 0.5
+
+        self.scale_input = lambda x: (np.asfarray(x[1:]) / 255.0 * 0.99) + 0.01
+    
+    def train(self, inputs_list, targets_list):
+        # convert input and target lists into column vectors
+        inputs = np.array(inputs_list, ndmin=2).T
+        targets = np.array(targets_list, ndmin=2).T
+
+        # calculate final outputs forwards layer by layer
+        hidden_inputs = np.dot(self.w_ih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = np.dot(self.w_ho, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+
+        # calculate errors backwards layer by layer
+        output_errors = targets - final_outputs
+        hidden_errors = np.dot(self.w_ho.T, output_errors)
+
+        # update weights of each weight matrix
+        self.w_ho += self.lr * np.dot((output_errors * final_outputs * (1.0 - final_outputs)), np.transpose(hidden_outputs))
+        self.w_ih += self.lr * np.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), np.transpose(inputs))
+
+    def query(self, inputs_list):
+        # convert input list into a column vector
+        inputs = np.array(inputs_list, ndmin=2).T
+
+        # calculate final outputs forwards layer by layer
+        hidden_inputs = np.dot(self.w_ih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = np.dot(self.w_ho, hidden_outputs)
+        final_ouputs = self.activation_function(final_inputs)
+
+        return final_ouputs
---
+++ mnist_handwritten_digits_test3.py
@@ -0,0 +1,71 @@
+import mnist_handwritten_digits
+import numpy as np
+import os
+
+# - each input graphic has 28x28 pixels that equals the number of nodes in the
+# input layer
+# - the hidden layer should have a number of nodes between the input and
+# output layer that the network is able to reduce and compress the information
+# given as output by the previous layer, but it must not be to low because
+# then there is not enough capacity for learning, on the other side the
+# calculation lasts to long if the number of nodes in the hidden layer is high
+# - the output is a number between 0 and 9, i.e. one node for each valid output
+# number in the output layer
+# - learning rate (alpha) can be a number between 0 and 1, but the smaller the
+# alpha, the less (slower) the current weights are corrected by the delta
+# (correction of the weights by the gradient descent), high values for alpha
+# can force an 'overshooting' effect, low values slow down the step range of
+# the gradient descent when finding the minimum of the error function (a good
+# alpha can be found by 'trial and error')
+nn = mnist_handwritten_digits.neuralNet(28**2, 200, 10, 0.1)
+
+# optional: define a path where data files are located
+path_to_data = "."
+small = False
+filename_training = "mnist_train_100.csv" if small else "mnist_train.csv"
+filename_test = "mnist_test_10.csv" if small else "mnist_test.csv"
+
+# read in training set
+data_file = open(os.path.join(path_to_data, filename_training), "r")
+data_training = data_file.readlines()
+data_file.close()
+
+# read in test set
+data_file = open(os.path.join(path_to_data, filename_test), "r")
+data_test = data_file.readlines()
+data_file.close()
+
+# define target vectors
+targets = {}
+onodes = 10
+for node in range(onodes):
+    target = np.zeros(onodes) + 0.01
+    target[node] = 0.99
+    targets[str(node)] = target
+
+# train the neural net with training set with mutiple epochs
+# the more epochs the more the machine can learn but that holds the danger
+# of overfitting (machine is used to training data and can't handle unknown
+# test data anymore)
+# the higher the number of epochs is, the lower the learning rate can be set
+# (if we go much more steps within the gradient descent, the range per step
+# can be shorter therefore)
+epochs = 5
+for _ in range(epochs):
+    for record in data_training:
+        training_data = record.split(',')
+        # optional for printing: resize the input array to a 28x28 matrix
+        #image_array = np.asfarray(training_data[1:]).reshape((28,28))
+        scaled_input = nn.scale_input(training_data)
+        nn.train(scaled_input, targets[training_data[0]])
+
+# test the neural net with test set and print the calculated output
+scorecard = []
+for record in data_test:
+    test_data = record.split(',')
+    scaled_input = nn.scale_input(test_data)
+    predicted_label = np.argmax(nn.query(scaled_input))
+    true_label = int(test_data[0])
+    scorecard.append(1 if predicted_label == true_label else 0)
+
+print("Accuracy of " + str(sum(scorecard) / len(scorecard)))
