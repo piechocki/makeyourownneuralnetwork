c71b228a65181e9f2655db496468c57a6a79314d
piechocki <34336439+piechocki@users.noreply.github.com>
2019-06-17 11:19:55 +0200

    Refactor test method, save pngs to file optionally

--- mnist_handwritten_digits_test2.py
+++ mnist_handwritten_digits_test2.py
@@ -1,27 +1,32 @@
 import mnist_handwritten_digits
-import scipy.misc, os, re, imageio
+import os, re, imageio, skimage.transform
 import numpy as np
-import skimage.transform
+from PIL import Image
 
 nn = mnist_handwritten_digits.neuralNet(28**2, 200, 10, 0.1)
 nn.load_weights()
 
 scorecard = []
-path_to_png = "C:\\Users\\marti\\Documents\\GitHub\\makeyourownneuralnetwork\\mnist_dataset\\PNG"
+path_to_png = ".\\mnist_dataset\\PNG"
+save_pngs = False
 pngs = [png for png in os.listdir(path_to_png) if re.match(r'.*\.png$', png)]
 
 for png in pngs:
     img_array = imageio.imread(os.path.join(path_to_png, png), as_gray=True)
-    x, y = img_array.shape
-    size = min(x, y)
-    #img_square = scipy.misc.imresize(img_array,(size,size))
-    img_square = scipy.misc.imresize(img_array,(28,28))
-    #img_square = skimage.transform.resize(img_array,(28,28),anti_aliasing=False)
-    #img_square = skimage.transform.downscale_local_mean(img_array,(28,28))
+    img_square = skimage.transform.resize(img_array,(28,28),anti_aliasing=False)
+    # x, y = img_array.shape
+    # size = min(x, y)
+    # img_square = skimage.transform.resize(img_array,(size,size))
+    # img_square = skimage.transform.downscale_local_mean(img_array,(28,28))
     img_data = 255.0 - img_square.reshape(784)
     scaled_input = (img_data / 255.0 * 0.99) + 0.01
     predicted_label = np.argmax(nn.query(scaled_input))
     true_label = int(png[-5])
     scorecard.append(1 if predicted_label == true_label else 0)
+
+    # optional: save read and resized image back to file
+    if save_pngs:
+        img = Image.fromarray(img_square)
+        img.convert("L").save(os.path.join(path_to_png + "_square", png))
 
 print("Accuracy of " + str(sum(scorecard) / len(scorecard)))
